9c66f6ae0e3574d3aafb4006e656866c860e91a1
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-03-17 13:47:11 -0400

    Arrange Progress Label / add collectionView to the pull up view

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -26,6 +26,9 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate { //inherit the deleg
     var spinner: UIActivityIndicatorView? //spinner to let user know downloading imgs in progress
     var progressLabel: UILabel? //progress label
     
+    var flowLayout = UICollectionViewFlowLayout() //if we make a collection view programmatically, we need to make a flowLayout for it.
+    var collectionView: UICollectionView? //collection view programmatically
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +36,16 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate { //inherit the deleg
         locationManager.delegate = self //set this to be delegate of locationManager
         configureLocationServices() //run this as soon as the app loads
         addDoubleTap() //call it
+        
+        
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout) //instantiate
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell") //to use it as a cell class, use .self at the end
+        collectionView?.delegate = self //delegate
+        collectionView?.dataSource = self //datasource, then conform to them to make it work.
+        collectionView?.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        
+        pullUpView.addSubview(collectionView!) //to add collectionview in the pullupView
     }
 
     
@@ -64,15 +77,35 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate { //inherit the deleg
         }
     }
     
-    func addSpinner() {
-        spinner = UIActivityIndicatorView() //instantiat\
+    func addSpinner() { //spinner to let user know downloading imgs in progress
+        spinner = UIActivityIndicatorView() //instantiate
         spinner?.center = CGPoint(x: (screensize.width / 2) - ((spinner?.frame.width)! / 2), y: 150) // divide by the width to be center ; 300 tall, divide by 2 to be centered vertically
         spinner?.activityIndicatorViewStyle = .whiteLarge
         spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         spinner?.startAnimating() //start animating
-        pullUpView.addSubview(spinner!) //add it to view
+        collectionView?.addSubview(spinner!) //add it to view
     }
     
+    func removeSpinner() { //to remove spinner
+        if spinner != nil { //if yes spinner, remove
+            spinner?.removeFromSuperview()
+        }
+    }
+    
+    func addProgressLabel() { //progress label to track activity progress
+        progressLabel = UILabel() //instantiate
+        progressLabel?.frame = CGRect(x: (screensize.width / 2) - 100, y: 175, width: 200, height: 40) // gotta divide width by 2, and by the length of the rectangle to center it
+        progressLabel?.font = UIFont(name: "Avenir Next", size: 18)
+        progressLabel?.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        progressLabel?.textAlignment = .center
+        collectionView?.addSubview(progressLabel!) //add it
+    }
+    
+    func removeProgressLabel() { //to remove prgLabel
+        if progressLabel != nil { //if yes prgLabel, remove
+            progressLabel?.removeFromSuperview()
+        }
+    }
     //IBactions
     @IBAction func centerMapBtnPressed(_ sender: Any) {
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse { //to check to see if we have access to location services
@@ -99,15 +132,19 @@ extension MapVC: MKMapViewDelegate { //conform to mapview delegate; another plac
     
     func centerMapOnUserLocation() { //center the map on the user's location
         guard let coordinate = locationManager.location?.coordinate else { return } //if we have location, show coordinates, if not, return
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0 , regionRadius * 2.0 ) // we have to multiply the regionradius by 2.0 because it's only one direction but we want 1000 meters in both directions;we're gonna set how wide we want the radius to be
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0 , regionRadius * 2.0 ) // we have to multiply the regionradius by 2.0 because it's only one direction but we want 1000 meters in both directions;we're gonna set how wide we want the radius to be around the center location
         mapView.setRegion(coordinateRegion, animated: true) //to set it
     }
     
     @objc func dropPin(sender: UITapGestureRecognizer) { //func to add pin, use this to set up tapgesture
         removePin() //remove pin before we add a new one
+        removeSpinner()  //to remove spinner everytime we drop a new pin
+        removeProgressLabel() //to remove progressLabel everytime we drop a new pin
+        
         animateViewUp() //call animate view, when we drop a pin.
-        addSwipe()
-        addSpinner()
+        addSwipe() // add swipe to hide the photo view down
+        addSpinner() //add spinner once we drop pin
+        addProgressLabel() //add label once we drop pin
         
         let touchPoint = sender.location(in: mapView)   //TouchPoint-where we touch on the map, something will happen in that exact location
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
@@ -141,6 +178,26 @@ extension MapVC: CLLocationManagerDelegate { //conform to locationManager delega
         centerMapOnUserLocation()
     }
 }
+
+extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource { //conform to these protocols to make collection view work
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // number of items in array
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) ->
+        UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell //gotta add an identifier so it knows what you're talking about
+            return cell!
+    }
+    
+}
+
 
 
 
---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Johnny Perdomo on 3/17/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) { //gotta override init to use photocell
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init coder has not been implemented")
+    }
+    
+    
+    
+}
